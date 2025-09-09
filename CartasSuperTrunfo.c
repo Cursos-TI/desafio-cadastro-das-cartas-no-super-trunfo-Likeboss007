55e7a6f77ff8fa113aff2715c069368ab0a772a1
Maicon Douglas Barbosa do Amaral <maiconbobsp@gmail.com>
2025-09-09 14:09:36 +0000

    CartasSuperTrunfo Github

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,96 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+printf("========================================================\n");
+printf("CARTAS SUPER TRUNFO\n");
+printf("Desenvolvido Por Maicon Douglas 2025\n");
+printf("Versão 1.0\n");
+printf("08/09/2025\n");
+printf("Descrição: Jogo de cartas Super Trunfo\n");
+printf("========================================================\n");
 
-    return 0;
+printf("Bem-vindo ao jogo de Cartas Super Trunfo\n");
+printf("Neste Jogo, voce ira competir com cartas de diferentes categorias\n");
+printf("Cada carta possui atributos que serão essenciais para determinar o vencedor\n");
+printf("Prepare se para uma partida emocionante!!!\n");
+
+
+//Declaração das variáveis para a primeira carta
+char estado1, codigo1[10], cidade1[20];
+int populacao1, pontos_turisticos1;
+float tamanho1, pib1;
+
+//Declaração das variáveis para a segunda carta
+char estado2, codigo2[10], cidade2[20];
+int população2, pontos_turisticos2;
+float tamanho2, pib2;
+
+//Cadastro Primeira Carta
+printf("=== CADASTRO DA PRIMEIRA CARTA ===\n");
+
+printf("Digite o Estado: (A-H) \n");
+scanf ("%s", &estado1);
+
+printf ("Digite o código da carta: \n");
+scanf ("%d", &codigo1);
+
+printf("Digite o nome da cidade (EX: Sao_Paulo): \n");
+scanf ("%s", &cidade1);
+
+printf("Digite a população: \n");
+scanf("%d", &populacao1);
+
+printf("Digite o numero de pontos turisticos: \n");
+scanf("%d", &pontos_turisticos1);
+
+printf("Digite o Tamanho: \n");
+scanf("%f", &tamanho1);
+
+printf("Digite o pib: \n");
+scanf("%f", &pib1);
+
+//Cadastro Segunda Carta
+printf("=== CADASTRO DA SEGUNDA CARTA ===\n");
+
+printf("Digite o Estado (A-H): \n");
+scanf("%s", &estado2);
+
+printf("Digite o codigo da carta: \n");
+scanf("%d", &codigo2);
+
+printf("Digite o nome da cidade (EX: Sao_Paulo): \n");
+scanf("%s", &cidade2);
+
+printf("Digite a população: \n");
+scanf("%d", &população2);
+
+printf("Digite o numero de pontos turísticos: \n");
+scanf("%d", &pontos_turisticos2);
+
+printf("Digite o tamanho2: \n");
+scanf("%f", &tamanho2);
+
+printf("Digite o pib: \n");
+scanf("%f", &pib2);
+
+//Exibição das Informações da Primeira carta
+printf(" \n=== CARTA 1 === \n");
+printf("Estado1: %s \n", &estado1);
+printf("Cidade1: %s \n", &cidade1);
+printf("População1: %d \n", &populacao1);
+printf("Pontos_turisticos1: %d \n", &pontos_turisticos1);
+printf("Area1: %d \n", &tamanho1);
+printf("PIB1: %d \n", &pib1);
+//Exibição das Informações da Segunda carta
+printf(" \n=== CARTA 2 === \n");
+printf("Estado2: %s \n", &estado2);
+printf("Codigo2: %d \n", &codigo2);
+printf("Cidade2: %s \n", &cidade2);
+printf("População2: %d \n", &população2);
+printf("Pontos_turisticos2: %d \n", &pontos_turisticos2);
+printf("tamanho2: %d \n", &tamanho2);
+printf("PIB2: %d \n", &pib2);
+
+
+return 0;
 }
